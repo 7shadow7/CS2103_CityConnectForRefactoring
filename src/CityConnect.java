708a8a4a7f1dd560367d4d98aac80c0d94a1f0de
unknown <premmybird@gmail.com>
2016-01-25 19:07:07 +0800

    removed showtoUser method

--- src/CityConnect.java
+++ src/CityConnect.java
@@ -99,12 +99,12 @@ public class CityConnect {
 	 * ====================================================================
 	 */
 	public static void main(String[] args) {
-		showToUser(WELCOME_MESSAGE);
+		System.out.println(WELCOME_MESSAGE);
 		while (true) {
 			System.out.print(MESSAGE_ENTER_COMMAND);
 			String userCommand = scanner.nextLine();
 			String feedback = executeCommand(userCommand);
-			showToUser(feedback);
+			System.out.println(feedback);
 		}
 	}
 
@@ -115,10 +115,6 @@ public class CityConnect {
 	 * that is referenced by the method above.
 	 * ====================================================================
 	 */
-
-	private static void showToUser(String text) {
-		System.out.println(text);
-	}
 
 	public static String executeCommand(String userCommand) {
 		String trimmedUserCommand = userCommand.trim();
